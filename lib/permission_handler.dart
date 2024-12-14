65f2c7d277c3f6c3df17a27e7b580714ce02e29c
roll1226 <tomo_yasu_4724@icloud.com>
2024-12-15 07:54:43 +0900

    feat: コメント削除

--- lib/permission_handler.dart
+++ lib/permission_handler.dart
@@ -3,9 +3,7 @@ import 'package:permission_handler/permission_handler.dart';
 Future<void> requestCameraPermission() async {
   final status = await Permission.camera.request();
   if (status.isGranted) {
-    // カメラ使用許可が取得できた場合の処理
   } else if (status.isPermanentlyDenied) {
-    // カメラ使用許可が永久に拒否された場合の処理
     openAppSettings();
   }
 }
